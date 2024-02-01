ec0813e8154c75476d6edaf87dfe269ad0d5b161
RahulBisht02 <rahul.bisht@aidetic.in>
2024-02-01 13:33:09 +0530

    calculateSimilarity

--- src/lib/calculateSimilarity.js
+++ src/lib/calculateSimilarity.js
@@ -1,26 +1,36 @@
 function calculateSimilarity(userPrefernces, gpttags, producttags) {
   //   const commonElements = userPrefernces.filter((preference) => gpttags.includes(preference));
   //   return (commonElements.length / userPrefernces.length) * 100;
+  const styles = [
+    'Classic and Timeless',
+    'Comfortable Yet Stylish',
+    'Premium Brands',
+    'Minimalistic',
+    'Practical',
+    'Chill',
+    'Bougie',
+  ];
 
-  let relationshipPercentage = gpttags[0].relationship.includes(userPrefernces.relationship) ? 100 : 0;
-  let occasionPercentage = gpttags[0].occasion.includes(userPrefernces.Occassion) ? 100 : 0;
+  let relationshipPercentage = producttags.includes(userPrefernces.relationship) ? 100 : 0;
+  let occasionPercentage = producttags.includes(userPrefernces.Occassion) ? 100 : 0;
 
-  
   let interestPercentage = 0;
   let stylePercentage = 0;
-  let 
+
   for (let i = 0; i < userPrefernces.preferences.length; i++) {
-    if (gpttags[0].style.includes(userPrefernces.preferences[i])) {
+    if (producttags.includes(userPrefernces.preferences[i])) {
       stylePercentage += 100;
-    }else if(gpttags[0].interest.includes(userPrefernces.preferences[i])){
+    } else if (producttags.includes(userPrefernces.preferences[i])) {
       interestPercentage += 100;
     }
   }
-  interestPercentage /= userPrefernces.preferences.length;
-  stylePercentage /= userPrefernces.preferences.length;
+  let productStyles = styles.filter((item) => producttags.include(item));
+  let productinterest = styles.filter((item) => !producttags.include(item));
+  interestPercentage /= productinterest.length;
+  stylePercentage /= productStyles.length;
 
-
-  let overallPercentage = 0.4 * occasionPercentage + 0.3 * relationshipPercentage + 0.2 * interestPercentage + 0.1 * stylePercentage;
+  let overallPercentage =
+    0.4 * occasionPercentage + 0.3 * relationshipPercentage + 0.2 * interestPercentage + 0.1 * stylePercentage;
 
   return overallPercentage;
 }
