1bd0a169eac773b16f824f015dbc55cbca908f86
aflah-aidetic <aflah.palora@aidetic.in>
2024-03-19 17:41:52 +0530

    i-101: rem index block

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -7,15 +7,15 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').then((registration) => {
-      console.log('ServiceWorker registration successful:', registration);
-    }, (error) => {
-      console.log('ServiceWorker registration failed:', error);
-    });
-  });
-}
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+//       console.log('ServiceWorker registration successful:', registration);
+//     }, (error) => {
+//       console.log('ServiceWorker registration failed:', error);
+//     });
+//   });
+// }
 
 root.render(
   // <React.StrictMode>
