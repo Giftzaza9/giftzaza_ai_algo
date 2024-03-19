cd8c0b9f4a67d284ba696f46daa6352586dc6915
aflah-aidetic <aflah.palora@aidetic.in>
2024-03-19 18:02:55 +0530

    rem sw

--- frontend/public/service-worker.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-restricted-globals */
-self.addEventListener('install', (event) => {
-  console.log('Service worker installed');
-});
-
-self.addEventListener('activate', (event) => {
-  console.log('Service worker activated');
-});
