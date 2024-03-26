3c2272db9b5e3d45b7d09b4e5b0192abee1c8368
aflah-aidetic <aflah.palora@aidetic.in>
2024-03-26 20:08:28 +0530

    Mini-Fix

--- frontend/src/components/shared/BottomNav/index.tsx
+++ frontend/src/components/shared/BottomNav/index.tsx
@@ -7,6 +7,7 @@ import { useNavigate } from 'react-router-dom';
 import { useMediaQuery } from '@mui/material';
 import { bottomNavIcons, iphoneSeCondition } from '../../../constants/constants';
 import { theme } from '../../../utils/theme';
+import { userStore } from '../../../store/UserStore';
 
 const bottomNavStyles = {
   position: 'fixed',
@@ -22,6 +23,8 @@ export const BottomNav = observer(() => {
   const isSmallScreen = useMediaQuery(iphoneSeCondition);
   const navigate = useNavigate();
   const { isVisible } = bottomNavState;
+  const { user } = userStore;
+  const token = !!localStorage.getItem('access_giftalia');
   const [value, setValue] = React.useState('home');
 
   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
@@ -32,7 +35,7 @@ export const BottomNav = observer(() => {
     <BottomNavigation
       value={value}
       onChange={handleChange}
-      sx={{ ...bottomNavStyles, display: isVisible ? { xs: 'flex' } : { xs: 'none' } }}
+      sx={{ ...bottomNavStyles, display: isVisible && user && token ? { xs: 'flex' } : { xs: 'none' } }}
     >
       {bottomNavIcons?.map(({ Icon, href, value }) => (
         <BottomNavigationAction
--- frontend/src/store/BottomNavState.ts
+++ frontend/src/store/BottomNavState.ts
@@ -2,9 +2,8 @@ import { action, makeObservable, observable } from 'mobx';
 import { bottomNavHidePaths } from '../constants/constants';
 
 const path = window.location.pathname;
-const token = !!localStorage.getItem('access_giftalia')
 export class BottomNavState {
-  isVisible: boolean = (!token && bottomNavHidePaths.some((el) => path.includes(el))) ? false : true;
+  isVisible: boolean = (bottomNavHidePaths.some((el) => path.includes(el))) ? false : true;
   constructor() {
     makeObservable(this, {
       isVisible: observable,
