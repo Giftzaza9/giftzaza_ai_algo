5588992873b381eaf826f666e76b40c7a806f172
aflah-aidetic <aflah.palora@aidetic.in>
2024-03-26 19:57:50 +0530

    Minor fixes

--- frontend/src/store/BottomNavState.ts
+++ frontend/src/store/BottomNavState.ts
@@ -2,13 +2,9 @@ import { action, makeObservable, observable } from 'mobx';
 import { bottomNavHidePaths } from '../constants/constants';
 
 const path = window.location.pathname;
+const token = !!localStorage.getItem('access_giftalia')
 export class BottomNavState {
-  isVisible: boolean = bottomNavHidePaths.some((el) => {
-    console.log(path);
-    return !path.includes(el);
-  })
-    ? true
-    : false;
+  isVisible: boolean = (!token && bottomNavHidePaths.some((el) => path.includes(el))) ? false : true;
   constructor() {
     makeObservable(this, {
       isVisible: observable,
