50f44397ad8257344dbe530aa4a55f48804c6b3b
aflah-aidetic <aflah.palora@aidetic.in>
2024-04-23 14:22:16 +0530

    Added error log to DB

---
+++ backend/src/routes/v1/errorLog.route.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const validate = require('../../middlewares/validate');
+const auth = require('../../middlewares/auth');
+const { rightsEnum } = require('../../config/roles');
+const { errorLogValidation } = require('../../validations');
+const { errorLogController } = require('../../controllers');
+
+const router = express.Router();
+
+router
+  .route('/:id')
+  .get(auth(rightsEnum.MANAGE_ERROR), validate(errorLogValidation.getErrorLog), errorLogController.getErrorLog)
+  .delete(auth(rightsEnum.MANAGE_ERROR), validate(errorLogValidation.deleteErrorLog), errorLogController.deleteErrorLog);
+
+router
+  .route('/')
+  .get(auth(rightsEnum.MANAGE_ERROR), validate(errorLogValidation.getErrorLogs), errorLogController.getErrorLogs)
+  .post(validate(errorLogValidation.createErrorLog), errorLogController.createErrorLog);
+
+module.exports = router;
